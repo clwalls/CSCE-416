0f09f968e9743fb1b9572d59e1d8349ac9540d79
dennisperea <54592924+dennisperea@users.noreply.github.com>
2019-11-14 10:51:58 -0500

    Updated Objectives
    More objectives and a better understanding of how client works

--- la4/StudentClientStrategy.java
+++ la4/StudentClientStrategy.java
@@ -8,7 +8,7 @@ public class StudentClientStrategy implements ClientStrategy{
 
     public StudentClientStrategy(){
         reset();
-        cwnd = 0;
+        cwnd = 1;
     }
 
     public void reset(){
@@ -25,25 +25,38 @@ public class StudentClientStrategy implements ClientStrategy{
     }
 
 
+    /*
+    * TODO: Increment cwnd everytime RTT increases. Program starts in cwnd
+    * Slow-start: if msg == ACK, cwnd = 2*cwnd
+    * Congestion Avoidance: 
+    *       if msg == ACK && cwnd <= ssthresh, cwnd++
+    *       if msg == ACK && cwnd > ssthresh, cwnd = cwnd + 1 / [ cwnd ] 
+    */
     
+    /* 
+    * What this method does: Builds a "file" that is the size of the server's message and then returns an array of ACK messages 
+    * Format: ACK #, "ACK"
+    */
     public List<Message> sendRcv(List<Message> serverMsgs){
         // For each message on the list, print the SEQ/ACK number and Message
         for(Message m : serverMsgs){
-            // This line builds up the file array to the size of the message's ACK/SEQ number size.
+            // This line builds up the file array to the size of the server message's ACK/SEQ number size.
             while(file.size() < m.num+1) file.add(null);
-            //At the designated ACK/SEQ num, add the string message
+            
+            //File == serverMsgs (copy)
             file.set(m.num,m.msg);
-            //PRINT message to console
             System.out.println(m.num+","+m.msg);
         }
 
-        //grab the first non null message
+        //Finds first non-null message, 
         int nextNeeded = 0;
         while(nextNeeded <file.size() && file.get(nextNeeded)!=null)
             ++nextNeeded;
 
+        
+        // ACK the message, add to list, return list of ACKs
+        // TODO: Send ${cwnd} amount of ACKs instead of one
         Message m=new Message(nextNeeded,"ACK");
-
         List<Message> ack = new ArrayList<Message>();
         ack.add(m);
         return ack;
