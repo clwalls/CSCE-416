bd0100485111c78a3c0e754e2eca7b38f0e30c1e
dennisperea <54592924+dennisperea@users.noreply.github.com>
2019-11-14 14:40:29 -0500

    fixed location of cwnd
    cwnd in wrong location

--- la4/StudentClientStrategy.java
+++ la4/StudentClientStrategy.java
@@ -2,42 +2,17 @@ import java.util.*;
 
 public class StudentClientStrategy implements ClientStrategy{
     ArrayList<String> file;
-    //(Congestion window) cwnd++ whenever RTT incereases, cuts in half when packet loss occurs
-    // Note: online examples have the sender doing congestion control and not the server
-    int cwnd;
-    int ssthresh;
     public StudentClientStrategy(){
         reset();
     }
 
     public void reset(){
         file = new ArrayList<String>();
-        cwnd = 1;
-        ssthresh = 20;
     }
 
     public List<String> getFile(){
         return file;
     }
-
-    //Call this function to cut cwindow in half, is called when packet loss is detected
-    public void halfCwnd(){
-        cwnd = cwnd/2;
-    }
-
-
-
-    /*
-    * TODO: Increment cwnd everytime RTT increases. Program starts in cwnd
-    * 
-    * Congestion Control: 
-    *       Slow Start: if msg == ACK && cwnd <= ssthresh, cwnd = 2*cwnd
-    *       Congestion Avoidance: if msg == ACK && cwnd > ssthresh, 
-    *             additive increase every RTT: cwnd++ 
-    *
-    *            TCP RENO: multiplative decrease, after packet loss: cwnd = cwnd /2, ssthresh = cwnd 
-    *            TCP TAHOE: set cwnd to 1
-    */
 
     /* 
     * What this method does: Builds a "file" that is the size of the server's message and then returns an array of ACK messages 
@@ -61,7 +36,6 @@ public class StudentClientStrategy implements ClientStrategy{
 
         
         // ACK the message, add to list, return list of ACKs
-        // TODO: Send ${cwnd} amount of ACKs instead of one
         Message m=new Message(nextNeeded,"ACK");
         List<Message> ack = new ArrayList<Message>();
         ack.add(m);
--- la4/StudentServerStrategy.java
+++ la4/StudentServerStrategy.java
@@ -4,6 +4,11 @@ public class StudentServerStrategy implements ServerStrategy{
     List<String> file;
     boolean[] acks;
 
+    //(Congestion window) cwnd++ whenever RTT incereases, cuts in half when packet loss occurs
+    // Note: online examples have the sender doing congestion control and not the server
+    int cwnd;
+    int ssthresh;
+
     public StudentServerStrategy(){
         reset();
     }
@@ -13,9 +18,21 @@ public class StudentServerStrategy implements ServerStrategy{
     }
 
     public void reset(){
-
-
+        cwnd = 1;
+        ssthresh = 20;
     }
+
+     /*
+    * TODO: Increment cwnd everytime RTT increases. Program starts in cwnd
+    * 
+    * Congestion Control: 
+    *       Slow Start: if msg == ACK && cwnd <= ssthresh, cwnd = 2*cwnd
+    *       Congestion Avoidance: if msg == ACK && cwnd > ssthresh, 
+    *             additive increase every RTT: cwnd++ 
+    *
+    *            TCP RENO: multiplative decrease, after packet loss: cwnd = cwnd /2, ssthresh = cwnd 
+    *            TCP TAHOE: set cwnd to 1
+    */
 
     public List<Message> sendRcv(List<Message> clientMsgs){
         for(Message m: clientMsgs){
